7300fec8f502b00d6de9583fa7c7ae638e3e8ee4
David <396866674@qq.com>
2021-08-12 12:41:03 +0800

    Site updated: 2021-08-12 12:40:50

---
+++ api/js/play.js
@@ -0,0 +1,11 @@
+jks = [
+	{url: "http://www.wocao.xyz/index.php?url=", title: "vip解析接口1"}, 
+    {url: "http://api.baiyug.vip/?url=", title: "vip解析接口2"}, 
+	{url: "http://jx.618g.com/?url=", title: "vip解析接口3"}, 
+	{url: "https://jx.1ff1.cn/?url=", title: "vip解析接口4"}, 
+	{url: "https://cdn.yangju.vip/k/?url=", title: "vip解析接口5"}, 
+	{url: "http://jx.cesms.cn/?url=", title: "vip解析接口8"}, 
+    {url: "https://jx.dy-jx.cn/?url=", title: "默认 万用接口3"},
+    {url: "https://api.sigujx.com/?url=", title: "默认 万用接口2"}, 
+    {url: "https://api.yueliangjx.com/?url=", title: "默认 万用接口1"}
+	];
